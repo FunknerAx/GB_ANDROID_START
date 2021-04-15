eb804faee1487e2721ca77caebaf39461bb4b0ea
FunknerAx <just.funkner@gmail.com>
2021-04-15 12:40:27 +0600

    Done 1/4

--- app/src/main/java/kz/funkner/myfirstapp/MainActivity.java
+++ app/src/main/java/kz/funkner/myfirstapp/MainActivity.java
@@ -2,7 +2,17 @@ package kz.funkner.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LinearLayout linearLayout = findViewById(R.id.linerLayout);
+        final TextView textView = findViewById(R.id.textView);
+        final EditText editText = findViewById(R.id.editText);
+        Button button = findViewById(R.id.button);
+        final Switch switchBut = findViewById(R.id.switchBut);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        final ToggleButton toggleButton = findViewById(R.id.toggleButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText());
+            }
+        });
+
+        switchBut.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    linearLayout.setBackgroundColor(R.color.colorWhite);
+                } else {
+                    linearLayout.setBackgroundColor(0x00000000);
+                }
+            }
+        });
+
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (!b) {
+                    textView.setTransitionAlpha(1.0f);
+                } else
+                    textView.setTransitionAlpha(0.0f);
+            }
+        });
+
     }
 }
